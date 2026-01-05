a05ce57fb2f6aee1dbfac90a6003e9fec88f346d
IO3683 <nu-dopustim@mail.ru>
2026-01-05 21:29:08 +0600

    Добавлена аннотация для валидации перечислений

---
+++ src/main/java/com/github/lemongrab32/util/EnumNamePatternValidator.java
@@ -0,0 +1,37 @@
+package com.github.lemongrab32.util;
+
+import com.github.lemongrab32.type.constraints.EnumNamePattern;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class EnumNamePatternValidator implements ConstraintValidator<EnumNamePattern, Enum<?>> {
+
+	public static final String CATEGORY_PATTERN = "CHILD|STUDENT|ADULT";
+	public static final String TYPE_PATTERN = "PRIVATE|ENTERPRISE";
+
+	private Pattern pattern;
+
+	@Override
+	public void initialize(EnumNamePattern annotation) {
+		try {
+			pattern = Pattern.compile(annotation.regexp());
+		} catch (PatternSyntaxException e) {
+			throw new IllegalArgumentException("Given regex is invalid", e);
+		}
+	}
+
+	@Override
+	public boolean isValid(Enum<?> value, ConstraintValidatorContext context) {
+		if (value == null) {
+			return true;
+		}
+
+		Matcher m = pattern.matcher(value.name());
+		return m.matches();
+	}
+
+}
