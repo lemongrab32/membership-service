8a59d420cb088f1f58ed5766bc94ebd8eff907f4
lemongrab32 <58136012+lemongrab32@users.noreply.github.com>
2026-01-09 09:21:34 +0600

    Обработка ошибок (#4)
    * Добавлено логирование и обработка ошибок к контроллерам
    
    * Добавлена аннотация для валидации перечислений
    
    * Добавлены новые сообщения для ошибок валидации
    
    * Добавлены новые сообщения ответов, небольшие исправления

--- src/main/java/com/github/lemongrab32/controller/dto/TariffRequest.java
+++ src/main/java/com/github/lemongrab32/controller/dto/TariffRequest.java
@@ -4,23 +4,34 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.github.lemongrab32.model.ClientCategory;
 import com.github.lemongrab32.model.ClientType;
+import com.github.lemongrab32.type.Messages;
+import com.github.lemongrab32.type.constraints.EnumNamePattern;
+import com.github.lemongrab32.util.EnumNamePatternValidator;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 @JsonNaming(PropertyNamingStrategies.KebabCaseStrategy.class)
 public record TariffRequest(
-	@Size(min = 2, max = 100)
-	@NotNull
+	@Size(min = 2, max = 100, message = "Количество символов в имени должно быть от 2 до 100")
+	@NotNull(message = Messages.VALIDATION_NOT_NULL_MESSAGE)
 	String name,
 
-	@Positive
-	@NotNull
+	@Positive(message = Messages.VALIDATION_POSITIVE_MESSAGE)
+	@NotNull(message = Messages.VALIDATION_NOT_NULL_MESSAGE)
 	Double basePrice,
 
-	@NotNull
+	@NotNull(message = Messages.VALIDATION_NOT_NULL_MESSAGE)
+	@EnumNamePattern(
+		regexp = EnumNamePatternValidator.CATEGORY_PATTERN,
+		message = Messages.VALIDATION_CATEGORY_INCORRECT_MESSAGE
+	)
 	ClientCategory category,
 
-	@NotNull
+	@NotNull(message = Messages.VALIDATION_NOT_NULL_MESSAGE)
+	@EnumNamePattern(
+		regexp = EnumNamePatternValidator.TYPE_PATTERN,
+		message = Messages.VALIDATION_TYPE_INCORRECT_MESSAGE
+	)
 	ClientType type
 ) {}
--- src/main/java/com/github/lemongrab32/type/Messages.java
+++ src/main/java/com/github/lemongrab32/type/Messages.java
@@ -9,9 +9,14 @@ public class Messages {
 
 	public final static String CALCULATION_SUCCESS_MESSAGE = "Стоимость абонемента успешно рассчитана";
 	public final static String MEMBERSHIP_SUCCESS_MESSAGE = "Абонемент успешно оформлен";
-	public final static String INVALID_CLIENT_OPTIONS_MESSAGE = "Тип или категория пользователя не соответствуют выбранному тарифу";
+	public final static String INVALID_CLIENT_OPTIONS_MESSAGE = "Указаны некорректные данные клиента";
 
 	public final static String PROPERTY_UPDATE_SUCCESS_MESSAGE = "Значение параметра успешно обновлено";
 	public final static String UNKNOWN_PROPERTY_MESSAGE = "Неизвестный параметр";
+
+	public final static String VALIDATION_NOT_NULL_MESSAGE = "Отсутствует значение поля";
+	public final static String VALIDATION_POSITIVE_MESSAGE = "Значение поля должно быть > 0";
+	public final static String VALIDATION_TYPE_INCORRECT_MESSAGE = "Указан несуществующий или пустой тип";
+	public final static String VALIDATION_CATEGORY_INCORRECT_MESSAGE = "Указана несуществующая или пустая категория";
 
 }
